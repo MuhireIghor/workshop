188b1166c63fc2d614b490f983962da884524b1f
MuhireIghor <muhireighor123@gmail.com>
2022-12-27 23:27:36 +0200

    [updates]:Input field component updates

--- @latest/src/COMPONENTS/InputField.jsx
+++ @latest/src/COMPONENTS/InputField.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
-const InputField = ({name,type,handler}) => {
+import '../css/sidebar.css'
+const InputField = ({name,type,handler,value,max,min,step,style}) => {
   return (
-    <div className='flex flex-col items-baseline'>
-<p>{name}</p>
-<input type={type} onChange={handler} />
+    <div className='flex flex-col items-baseline gap-2'>
+<p className='text-xl font-semibold'>{name}</p>
+<input type={type} onChange={handler} value={value} max={max} min={min} step={step} style={style} />
     </div>
   )
 }
