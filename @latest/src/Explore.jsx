7d31da18d6214f186a8b284948b1815d4fd9b529
MuhireIghor <muhireighor123@gmail.com>
2022-12-27 23:24:33 +0200

    [updates]:Explore page updates

--- @latest/src/Explore.jsx
+++ @latest/src/Explore.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { FiSearch,FiShoppingBag } from 'react-icons/fi';
+import Products from './COMPONENTS/Products';
 import SideBar from './COMPONENTS/SideBar';
 
 
 const Explore = () => {
     
   return (
- <div>
+ <div className='flex h-screen'>
+  <div className='basis-2/12'>
 <SideBar />
+  </div>
+  <div className='basis-9/12 mt-24'>
+<Products />
+  </div>
 
  </div>
 
