79af5ec35291297b05f20d0910a5a1e057a31479
MuhireIghor <muhireighor123@gmail.com>
2022-12-29 19:57:50 +0200

    updates

--- @latest/src/COMPONENTS/AdminProj.jsx
+++ @latest/src/COMPONENTS/AdminProj.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Admindash from './Admindash'
+import AdminProdList from './AdminProdList'
 
 const AdminProj = () => {
-  return (
-    <div>AdminProj</div>
-  )
+    return (
+        <div className='flex relative '>
+            <div className='basis-3/12 '>
+                <Admindash />
+            </div>
+            <div className='flex basis-8/12  bg-[red]'>
+              <AdminProdList />
+            </div>
+
+        </div>
+    )
 }
 
 export default AdminProj
