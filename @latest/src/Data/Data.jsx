e6414de3b1cdf41aabed2b1c1e82bcaae27ab62d
MuhireIghor <muhireighor123@gmail.com>
2022-12-29 18:15:45 +0200

    data set collection

---
+++ @latest/src/Data/Data.jsx
@@ -0,0 +1,159 @@
+export const dataset = [
+    {
+        imgUrl:"../../src/Assets/index-removebg-preview(1).png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50!",
+        price:"20000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index2-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50!",
+        price:"12000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50!",
+        price:"18000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50!",
+        price:"25000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index-removebg-preview(1).png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50!",
+        price:"14000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index2-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50!",
+        price:"16000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index3-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dsdds",
+        price:"22000",
+
+    },
+    {
+        imgUrl:"../../src/Assets/index4-removebg-preview.png",
+        description:"Online dress shopping has never been simpler! Shop the latest dress styles and trendy fashion at the best prices. Free shipping over $50! dssds",
+        price:"19000",
+
+    },
+   
+]
